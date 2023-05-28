224f97a17f1008155ca494e43ca089c194f3bd1a
Aaron Myles Landwehr <snaphat@gmail.com>
2023-05-27 20:50:19 -0400

    Add seafloor

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ class Program
     private const int FishBuffer = 12;
     private const int BottomBuffer = 7;
 
+    private static bool SeaFloorDrawn;
+
     static void Main()
     {
         Console.CursorVisible = false;
@@ -19,16 +21,90 @@ class Program
         List<Fish> fishesRight = GenerateFishes(false);
         List<Bubble> bubbles = GenerateBubbles();
 
+        // Draw the aquarium once at the beginning
+        DrawAquarium(fishesLeft, fishesRight, bubbles);
+
         while (true)
         {
-            UpdateFishes(fishesLeft, true);
-            UpdateFishes(fishesRight, false);
-            UpdateBubbles(bubbles);
+            UpdateAndRedrawFishes(fishesLeft, true);
+            UpdateAndRedrawFishes(fishesRight, false);
+            UpdateAndRedrawBubbles(bubbles);
 
-            DrawAquarium(fishesLeft, fishesRight, bubbles);
-
-            Thread.Sleep(100);
+            Thread.Sleep(50);
         }
+    }
+
+    static void DrawSeaFloor()
+    {
+        if (!SeaFloorDrawn)
+        {
+            int seaFloorStart = Console.WindowHeight - BottomBuffer;
+
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.SetCursorPosition(i, seaFloorStart);
+                Console.Write("~");
+            }
+
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                for (int j = seaFloorStart + 1; j < Console.WindowHeight; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write(Random.Next(0, 3) > 0 ? "'" : ".");
+                }
+            }
+
+            SeaFloorDrawn = true;
+        }
+    }
+
+    static void UpdateAndRedrawFishes(List<Fish> fishes, bool moveLeft)
+    {
+        foreach (Fish fish in fishes)
+        {
+            ClearAtPosition(fish.X, fish.Y, fish.Symbol.Length);
+            fish.Move(moveLeft);
+            DrawFish(fish);
+        }
+    }
+
+    static void UpdateAndRedrawBubbles(List<Bubble> bubbles)
+    {
+        foreach (Bubble bubble in bubbles)
+        {
+            ClearAtPosition(bubble.X, bubble.Y, bubble.Symbol.Length);
+            bubble.Rise();
+            DrawBubble(bubble);
+        }
+    }
+
+    static void ClearAtPosition(int x, int y, int length)
+    {
+        int seaFloorStart = Console.WindowHeight - BottomBuffer;
+        for (int i = 0; i < length; i++)
+        {
+            if (y < seaFloorStart)
+            {
+                Console.SetCursorPosition(x + i, y);
+                Console.Write(' ');
+            }
+        }
+    }
+
+    static void DrawFish(Fish fish)
+    {
+        int y = Math.Max(0, Math.Min(Console.WindowHeight - 1, fish.Y)); // Ensure Y coordinate is within valid range
+        Console.SetCursorPosition(fish.X, y);
+        Console.ForegroundColor = fish.Color;
+        Console.Write(fish.Symbol);
+    }
+
+    static void DrawBubble(Bubble bubble)
+    {
+        Console.SetCursorPosition(bubble.X, bubble.Y);
+        Console.ForegroundColor = bubble.Color;
+        Console.Write(bubble.Symbol);
     }
 
     static void DrawAquarium(List<Fish> fishesLeft, List<Fish> fishesRight, List<Bubble> bubbles)
@@ -37,27 +113,20 @@ class Program
 
         foreach (Fish fish in fishesLeft)
         {
-            int y = Math.Max(0, Math.Min(Console.WindowHeight - 1, fish.Y)); // Ensure Y coordinate is within valid range
-            Console.SetCursorPosition(fish.X, y);
-            Console.ForegroundColor = fish.Color;
-            Console.Write(fish.Symbol);
+            DrawFish(fish);
         }
 
         foreach (Fish fish in fishesRight)
         {
-            int x = fish.X + fish.Symbol.Length - 1;
-            int y = Math.Max(0, Math.Min(Console.WindowHeight - 1, fish.Y)); // Ensure Y coordinate is within valid range
-            Console.SetCursorPosition(x, y);
-            Console.ForegroundColor = fish.Color;
-            Console.Write(fish.Symbol);
+            DrawFish(fish);
         }
 
         foreach (Bubble bubble in bubbles)
         {
-            Console.SetCursorPosition(bubble.X, bubble.Y);
-            Console.ForegroundColor = bubble.Color;
-            Console.Write(bubble.Symbol);
+            DrawBubble(bubble);
         }
+
+        DrawSeaFloor();
     }
 
     static List<Fish> GenerateFishes(bool moveLeft)
@@ -121,10 +190,11 @@ class Program
         for (int i = 0; i < 30; i++)
         {
             int x = random.Next(2, Console.WindowWidth - 2);
-            int y = random.Next(2, Console.WindowHeight - 2);
+            // Make sure the bubbles start above the sea floor
+            int y = random.Next(2, Console.WindowHeight - BottomBuffer - 1);
             string bubbleSymbol = GenerateBubbleSymbol();
             ConsoleColor color = (ConsoleColor)random.Next(1, 16);
-            Bubble bubble = new Bubble(x, y, bubbleSymbol, color);
+            Bubble bubble = new Bubble(x, y, bubbleSymbol, color, BottomBuffer);
             bubbles.Add(bubble);
         }
 
@@ -229,13 +299,15 @@ class Bubble
     public int Y { get; private set; }
     public string Symbol { get; }
     public ConsoleColor Color { get; }
+    private int BottomBuffer { get; }
 
-    public Bubble(int x, int y, string symbol, ConsoleColor color)
+    public Bubble(int x, int y, string symbol, ConsoleColor color, int bottomBuffer)
     {
         X = x;
         Y = y;
         Symbol = symbol;
         Color = color;
+        BottomBuffer = bottomBuffer;
     }
 
     public void Rise()
@@ -243,7 +315,7 @@ class Bubble
         Y--;
         if (Y < 2)
         {
-            Y = Console.WindowHeight - 7;
+            Y = Console.WindowHeight - BottomBuffer - 1;
         }
     }
 }
